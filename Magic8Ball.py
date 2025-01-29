f45285186f4d5b3bc1dffd88f7e755465d9c6148
Justin031806 <justinralston@unomaha.edu>
2025-01-29 15:50:10 +0000

    finished

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -3,15 +3,26 @@
 #Date:
 #Assignment:
 
-#We will need random for this program, import to use this package.
-import random
+import random 
 
 def main():
   #Create a list of your responses.
+  answers=["not really","sure","okay","Very good","incorrect","try again","almost there","you did it"]
   print("Magic 8 Ball")
   #Prompt the user for their question.
 
+
   #Answer question randomly with one of the options from your earlier list.
+
+  num =random.random() #decimal 0-1
+  num=num*1000 #number 0-999 with decimals
+  num=int(num) # no more decimals
+  num=num%8 #0-7
+  
+
+  question=input("Ask me a question:")
+  print(answers[num])
+  
 
 
 if __name__ == '__main__':
